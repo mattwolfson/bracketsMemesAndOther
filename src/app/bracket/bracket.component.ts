60f821bfe7f313a2ac271ad614ffd64b878fb963
mattwolfson <mcw88@cornell.edu>
2017-05-06 14:28:28 -0400

    Created Advancing logic for each bracket position
    Team has to win every game before their furthest position

--- src/app/bracket/bracket.component.ts
+++ src/app/bracket/bracket.component.ts
@@ -10,6 +10,117 @@ export class BracketComponent implements OnInit {
 
   constructor() { }
 
+  public advance(pick) {
+  // 	 var replaceAt = function(index, replacement) {
+	 //    return this.substr(0, index) + replacement+ this.substr(index + replacement.length);
+		// }
+		var noTeamChosenSrc = 'src/img/bracket/nba/Solid_white.svg';
+  	var numberOfRounds = 4;
+  	var teamClasses = pick.parentNode.className;
+  	var roundElement = pick.parentNode.parentNode;
+  	var roundClasses = pick.parentNode.parentNode.className;
+  	var roundNumber = roundClasses.substring(roundClasses.indexOf('toGo')-1).charAt(0);
+  	var gameNumber = Number(teamClasses.substring(teamClasses.indexOf('game')).charAt(4));
+  	var teamNumber = Number(teamClasses.substring(teamClasses.indexOf('team')).charAt(4));
+  	var competingTeamNumber = teamNumber % 2 + 1;
+  	var competingTeamClass = 'game' + gameNumber + ' team' + competingTeamNumber;
+  	var competingTeam;
+  	var competingTeamSrc;
+
+	  var roundToCheckCompetingTeamIsNotIn = roundNumber - 2;
+	  if (roundToCheckCompetingTeamIsNotIn >=0) {
+	  	for (var i = 0; i < roundElement.childNodes.length; i++) {
+	  		if (roundElement.childNodes[i].className &&
+	  			roundElement.childNodes[i].className.includes(competingTeamClass)) {
+	  			competingTeam = roundElement.childNodes[i];
+	  			break;
+	  		}
+	  	}
+
+	  	//Find picture from competing team
+	  	for (var i = 0; i < competingTeam.childNodes.length; i++) {
+	  		if (competingTeam.childNodes[i].tagName == 'DIV') {
+	  			var teamLogoHolder = competingTeam.childNodes[i];
+		  		for (var i = 0; i < teamLogoHolder.childNodes.length; i++) {
+		  			if (teamLogoHolder.childNodes[i].tagName == 'IMG'){
+		  				competingTeamSrc = teamLogoHolder.childNodes[i].src;
+		  				break;
+		  			}
+		  		}
+	  		}
+	  	}
+
+	  	//Check if the competing team is in any future rounds
+	  	var newRoundClasses;
+	  	var roundIndex;
+	  	var roundClassIndex;
+	  	var newGameNumber;
+	  	var newTeamNumber;
+	  	var futureGameNumber;
+	  	var futureTeamNumber;
+	  	var newClass;
+	  	var futureTeam;
+	  	var needToCheckNextRound = true;
+		  var roundToCheckCompetingTeamIsNotIn = roundNumber - 2;
+		  newGameNumber = Math.round(gameNumber/2);
+		  Math.round(gameNumber/2) !== gameNumber/2 ? newTeamNumber = 1 : newTeamNumber = 2;
+
+	  	while (needToCheckNextRound && roundToCheckCompetingTeamIsNotIn >=0) {
+		  	needToCheckNextRound = false;
+		  	if (roundToCheckCompetingTeamIsNotIn > 0) {
+		  		roundIndex = roundClasses.indexOf(('toGo'))-1;
+		  		newRoundClasses = roundClasses.substring(0,roundIndex) + 
+		  			roundToCheckCompetingTeamIsNotIn +
+		  			roundClasses.substring(roundIndex + 1);
+		  		roundClassIndex = newRoundClasses.indexOf('round');
+		  		newRoundClasses = newRoundClasses.substring(0,roundClassIndex) +
+		  			newRoundClasses.substring(roundClassIndex + 7);
+		  		futureGameNumber = Math.round(newGameNumber/2);
+		  		Math.round(newGameNumber/2) !== newGameNumber/2 ? futureTeamNumber = 1 : futureTeamNumber = 2;
+		  		if (newRoundClasses.indexOf('east') >= 0 && roundToCheckCompetingTeamIsNotIn === 1) { 
+		  			futureTeamNumber = 2;
+		  		}
+		  		newGameNumber = futureGameNumber;
+		  		newTeamNumber = futureTeamNumber;
+		  		newClass = "game" + futureGameNumber + " team" + futureTeamNumber;
+		  	} else {
+		  		newRoundClasses = '0toGo';
+		  		newClass = "champion";
+		  	}
+
+	  		var newRoundElement = document.getElementsByClassName(newRoundClasses)[0];
+	  		futureTeam = null;
+	  		for (var i = 0; i < newRoundElement.childNodes.length; i++) {
+		  		if (newRoundElement.childNodes[i]['className'] &&
+		  			newRoundElement.childNodes[i]['className'].includes(newClass)) {
+		  			futureTeam = newRoundElement.childNodes[i];
+		  			break;
+		  		}
+		  	}
+
+		  	if(!futureTeam) { console.log('no future team found'); }
+
+		  	//Find picture from competing team
+		  	for (var i = 0; i < futureTeam.childNodes.length; i++) {
+		  		if (futureTeam.childNodes[i].tagName == 'DIV') {
+		  			var teamLogoHolder = futureTeam.childNodes[i];
+			  		for (var i = 0; i < teamLogoHolder.childNodes.length; i++) {
+			  			if (teamLogoHolder.childNodes[i].tagName == 'IMG'){
+			  				if (teamLogoHolder.childNodes[i].src === competingTeamSrc) {
+			  					teamLogoHolder.childNodes[i].src = noTeamChosenSrc;
+			  					roundToCheckCompetingTeamIsNotIn--;
+			  					needToCheckNextRound = true;
+			  				}
+			  				break;
+			  			}
+			  		}
+		  		}
+		  	}
+		  }
+		}
+  };
+
+
   ngOnInit() {
   	var i,
         j,
